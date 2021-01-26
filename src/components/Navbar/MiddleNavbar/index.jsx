a025cbe66cc3f1a3b0b07c2411293a2d0a61f7c3
Nejc <nejcrogelsek0@gmail.com>
2021-01-26 22:16:22 +0100

    fix topNavbar and add HTML middleNavbar

--- src/components/Navbar/MiddleNavbar/index.jsx
+++ src/components/Navbar/MiddleNavbar/index.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import Logo from "../../../assets/images/logo.svg";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+import SearchIcon from "@material-ui/icons/Search";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const MiddleNavbar = () => {
-  return <div>middle</div>;
+  const [search, setSearch] = useState("");
+  const [isMobile, setIsMobile] = useState(false);
+
+  const startSearch = () => {
+    console.log("jojojojojoo");
+  };
+  return (
+    <div id='middleNavbar'>
+      {isMobile && (
+        <div className='menu-wrapper'>
+          <MenuIcon />
+        </div>
+      )}
+      <div className='header-logo'>
+        <img src={Logo} alt='Logo' />
+      </div>
+      <div className='search-box'>
+        <input
+          type='text'
+          name='search'
+          id='search'
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type='submit' onClick={startSearch}>
+          Search
+        </button>
+      </div>
+      <div className='middle-buttons'>
+        {isMobile && <SearchIcon />}
+        <ShoppingCartOutlinedIcon />
+      </div>
+    </div>
+  );
 };
 
 export default MiddleNavbar;
