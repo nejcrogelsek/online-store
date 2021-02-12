aa6c66399a15ddafcb97cecdba4fe0d7e41cb7a6
Nejc <nejcrogelsek0@gmail.com>
2021-02-12 15:58:46 +0100

    category first level

--- src/components/Category/FirstLevelCategory/FirstLevelCategory.jsx
+++ src/components/Category/FirstLevelCategory/FirstLevelCategory.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import { Link } from "@material-ui/core";
+import React from "react";
+import Image1 from "../../../assets/images/breadcrumb/art2.jpg";
 
 const FirstLevelCategory = () => {
-    return (
-        <div id="cat-lvl1">
-            <div className="row">
-                1 lvl
-            </div>
+  return (
+    <div class='cat-lvl1'>
+      <div className='item-grid'>
+        <div className='item-box'>
+          <Link to='/category/subcat'>
+            <img src={Image1} alt='category picture' />
+            <span className='title'>Moška obutev</span>
+          </Link>
         </div>
-    )
-}
+        <div className='item-box'>
+          <Link to='/category/subcat'>
+            <img src={Image1} alt='category picture' />
+            <span className='title'>Moška obutev</span>
+          </Link>
+        </div>
+        <div className='item-box'>
+          <Link to='/category/subcat'>
+            <img src={Image1} alt='category picture' />
+            <span className='title'>Moška obutev</span>
+          </Link>
+        </div>
+        <div className='item-box'>
+          <Link to='/category/subcat'>
+            <img src={Image1} alt='category picture' />
+            <span className='title'>Moška obutev</span>
+          </Link>
+        </div>
+        <div className='item-box'>
+          <Link to='/category/subcat'>
+            <img src={Image1} alt='category picture' />
+            <span className='title'>Moška obutev</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default FirstLevelCategory
+export default FirstLevelCategory;
